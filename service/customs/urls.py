1c5fe478b7fd6ffda2e5a0eaa432f0a70b81531f
Joy <mbuvimumbuambua99@gmail.com>
2024-11-28 14:09:32 +0300

    project flow

--- service/customs/urls.py
+++ service/customs/urls.py
@@ -6,9 +6,11 @@ from .views import *
 urlpatterns=[
     path('customers/',All_customers.as_view(),name='all customers'),
     path('customers/new/',Create_customers.as_view(),name='Create new consumer'),
+    path('customers/google/callback/', GoogleOAuthCallback.as_view(), name='google_oauth_callback'),
     path('orders/<int:customer_id>/',All_order.as_view(),name='Orders'),
     path('orders/new/',Create_orders.as_view(),name='Create new orders'),
     path('dashboard/', dashboard, name='dashboard'),  
+    path('create/', create_customer_page, name='create_customer_page'),
 
 ]
 
--- service/customs/views.py
+++ service/customs/views.py
@@ -1,46 +1,105 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.shortcuts import redirect,render
 from .models import Customers, Order
 from .serializers import CustomerSerializer, OrderSerializer
-# import africastalking
+from django.conf import settings
+from django.http import HttpResponseRedirect
+import requests
+import africastalking
 
 def dashboard(request):
     return render(request, 'dashboard.html')
 
-
-
-class All_customers(APIView):
-    permission_classes = [IsAuthenticated]
-    required_scopes = ['read'] 
-
-    def get(self, request):
-        try:
-            data = Customers.objects.all()
-            serializer = CustomerSerializer(data, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except :
-            return Response({'error': 'Unable to fetch customers'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+def create_customer_page(request):
+    return render(request, 'create_customer.html')
 
 class Create_customers(APIView):
     permission_classes = [AllowAny]
-    required_scopes = ['write', 'read']
 
     def post(self, request):
         try:
             serializer = CustomerSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save()
-                return Response({'message': 'Customer created successfully'}, status=status.HTTP_201_CREATED)
-            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception :
-            return Response({'error': 'An unexpected error occurred'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+             serializer.save()
+
+
+             client_id = settings.SOCIALACCOUNT_PROVIDERS['google']['APP']['client_id']
+             return redirect(
+                    f"https://accounts.google.com/o/oauth2/auth"
+                    f"?client_id={client_id}"
+                    f"&redirect_uri=http://localhost:8000/customs/customers/google/callback/"
+                    f"&response_type=code&scope=openid profile email"
+                )
+
+        
+
+
+        except:   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class GoogleOAuthCallback(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        code = request.GET.get('code')
+
+        token_url = "https://oauth2.googleapis.com/token"
+        data = {
+            'code': code,
+            'client_id': settings.SOCIALACCOUNT_PROVIDERS['google']['APP']['client_id'],
+            'client_secret': settings.SOCIALACCOUNT_PROVIDERS['google']['APP']['secret'],
+            'redirect_uri': settings.LOGIN_REDIRECT_URL,
+            'grant_type': 'authorization_code',
+        }
+        response = requests.post(token_url, data=data)
+        # print("Token response status:", response.status_code)
+
+        token_data = response.json()
+        # print ('token response data',token_data)
+        access_token = token_data['access_token']
+        print(f"Access Token: {access_token}")  
+
+        user_info_url = "https://www.googleapis.com/oauth2/v3/userinfo"
+        headers = {'Authorization': f'Bearer {access_token}'}
+        user_info_response = requests.get(user_info_url, headers=headers)
+
+        auhenticated_customer=user_info_response.json()
+        name= auhenticated_customer.get('name')
+
+
+        customer= Customers.objects.get(name=name)
+
+        refresh=RefreshToken.for_user(customer)
+
+        return  Response({
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        })
+
+
+
+
+class All_customers(APIView):
+    # permission_classes = [IsAuthenticated] 
+
+    def get(self, request):
+
+        
+        try:
+            data = Customers.objects.all()
+            serializer = CustomerSerializer(data, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': 'Unable to fetch customers'}, status=status.HTTP_500_INTERNAL_SERVER_ERROr)
+
 
 class All_order(APIView):
-    permission_classes = [IsAuthenticated]
-    required_scopes = ['read']
+    # permission_classes = [IsAuthenticated]
 
     def get(self, request, customer_id):
         try:
@@ -56,7 +115,6 @@ africastalking.initialize(settings.AFRICASTALKING_USERNAME, settings.AFRICASTALK
 sms = africastalking.SMS
 class Create_orders(APIView):
     permission_classes = [IsAuthenticated]
-    required_scopes = ['read', 'write']
 
     def post(self, request):
         try:
